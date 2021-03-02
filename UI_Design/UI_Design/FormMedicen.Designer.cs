29c44de12cfca0b862d501ce9bb831cb231b1d25
Sergey Kiselev <kiselev.itstep@gmail.com>
2021-03-02 21:06:14 +0200

    add class Medicina

--- UI_Design/UI_Design/FormMedicen.Designer.cs
+++ UI_Design/UI_Design/FormMedicen.Designer.cs
@@ -29,22 +29,37 @@ namespace UI_Design
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(945, 426);
+            this.dataGridView1.TabIndex = 0;
             // 
             // FormMedicen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(1310, 650);
+            this.ClientSize = new System.Drawing.Size(982, 528);
+            this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMedicen";
             this.Text = "FormMedicen";
             this.Load += new System.EventHandler(this.FormDocuments_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
---
+++ UI_Design/UI_Design/Medicina.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Design
+{
+    class Medicina
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+
+        [Required, MaxLength(60)]
+        public string FullName { get; set; }
+
+        [Required, MaxLength(60)]
+        public string Proff { get; set; }
+
+        public DateTime DatePriyom { get; set; }
+
+        public DateTime DateNextPriyom { get; set; }
+
+        public virtual ICollection<Imagge> Images { get; set; }
+    }
+}
